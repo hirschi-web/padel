49fa682ad228ec01349e8080f5c772b9fc8c4deb
hirschi-web <hirschi77@gmail.com>
2026-01-30 11:07:14 +0100

    Update save.php

--- save.php
+++ save.php
@@ -1,12 +1,7 @@
 <?php
-// Empfange JSON Daten vom POST Request
 $json = file_get_contents('php://input');
-
 if($json) {
-    // Speichere die Daten in data.txt
     file_put_contents('data.txt', $json);
-    echo "OK";
-} else {
-    echo "Keine Daten empfangen";
+    echo "Saved";
 }
 ?>
