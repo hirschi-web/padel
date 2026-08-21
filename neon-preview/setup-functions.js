19a191c2ce2ed3109d30e8b538ded5d6964a4439
hirschi-web <hirschi77@gmail.com>
2026-08-21 17:30:47 +0200

    Add Neon setup bridge for preview

---
+++ neon-preview/setup-functions.js
@@ -0,0 +1,3 @@
+// Neon preview bridge: replace Supabase client before loading the unchanged setup logic.
+document.write('<script src="/padel/neon-preview/padel-api.js?v=20260821-2"><\/script>');
+document.write('<script src="/padel/setup-functions.js?v=20260821-2"><\/script>');
