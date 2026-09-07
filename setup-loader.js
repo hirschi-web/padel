8c726044101a5149ecb4f36ad6024c2d7dfcbbfc
hirschi-web <hirschi77@gmail.com>
2026-09-07 14:24:42 +0200

    Bust setup asset cache

--- setup-loader.js
+++ setup-loader.js
@@ -5,8 +5,8 @@
     let h=await r.text();
     h=h.replace(/<script[^>]*src=["'][^"']*@supabase\/supabase-js[^"']*["'][^>]*><\/script>/i,'');
     h=h.replace('<head>','<head><base href="/padel/neon-preview/"><style>#neonDebugLog{display:none!important}</style>');
-    h=h.replace('<script src="setup-functions.js"></script>','<script src="/padel/setup-neon-bootstrap.js?v=20260824-2"><\/script>');
-    h=h.replace('</body>','<script src="/padel/setup-live-links.js?v=20260824-2"><\/script><script src="/padel/americano-court-fairness.js?v=20260831-1"><\/script></body>');
+    h=h.replace('<script src="setup-functions.js"></script>','<script src="/padel/setup-neon-bootstrap.js?v=20260907-2"><\/script>');
+    h=h.replace('</body>','<script src="/padel/setup-live-links.js?v=20260907-2"><\/script><script src="/padel/americano-court-fairness.js?v=20260907-2"><\/script></body>');
     document.open();
     document.write(h);
     document.close();
